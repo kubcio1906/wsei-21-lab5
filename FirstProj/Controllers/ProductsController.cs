d5ef8e5c45bcab5edc4a5df5d2c9e570dbbf07d8
Jakub Madej <jakub.madej1@microsoft.wsei.edu.pl>
2021-10-09 09:48:23 +0200

    lab2

---
+++ FirstProj/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using FirstProj.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstProj.Controllers
+{
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(ProductModel product)
+        {
+            var viewModel = new ProductStatsViewModel
+            {
+                NameLength = product.Name.Length,
+                DescriptionLength = product.Description.Length,
+            };
+            return View(viewModel);
+        }
+       
+    }
+}
---
+++ FirstProj/Models/ProductStatsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstProj.Models
+{
+    public class ProductStatsViewModel
+    {
+        public int NameLength { get; internal set; }
+        public int DescriptionLength { get; internal set; }
+    }
+}
